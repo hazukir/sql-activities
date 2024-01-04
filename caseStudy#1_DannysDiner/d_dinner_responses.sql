b4fb3e5fca2b015afa76ce6ef0c95951b795b2c3
Helder Varela <heldert4p@gmail.com>
2024-01-04 16:29:12 +0000

    Add solutions to questions 6 & 7. Alter the solution #5

--- caseStudy#1_DannysDiner/d_dinner_responses.sql
+++ caseStudy#1_DannysDiner/d_dinner_responses.sql
@@ -49,3 +49,93 @@ FROM (SELECT DISTINCT
 		ON S.product_id = M.product_id) AS subquery
 WHERE denseRank = 1;
 
+
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+WITH purchasedItems
+AS
+(SELECT -- Get the total for each product
+		S.product_id
+	   ,M.product_name
+	   ,COUNT(S.order_date) AS totalPurchases
+	FROM sales AS S
+	LEFT JOIN menu AS M
+		ON S.product_id = M.product_id
+	GROUP BY S.product_id
+			,M.product_name)
+SELECT -- Get the Most purchased Item (max value) of all purchases
+	UPPER(LEFT(product_name, 1)) + LOWER(SUBSTRING(product_name, 2, LEN(product_name))) AS product_name -- capitalize the first letter
+   ,totalPurchases
+FROM purchasedItems
+WHERE totalPurchases = (SELECT
+		MAX(totalPurchases)
+	FROM purchasedItems);
+
+
+-- 5. Which item was the most popular for each customer?
+
+
+WITH purchasedItems AS (
+    SELECT
+        S.customer_id,
+        M.product_name,
+        COUNT(S.order_date) AS totalCount,
+        ROW_NUMBER() OVER (PARTITION BY S.customer_id ORDER BY COUNT(S.order_date) DESC) AS rankOrders
+    FROM sales AS S
+    INNER JOIN menu AS M ON S.product_id = M.product_id
+    GROUP BY S.customer_id, M.product_name
+)
+SELECT customer_id, product_name, totalCount
+FROM purchasedItems
+WHERE rankOrders = 1;
+
+
+
+-- 6. Which item was purchased first by the customer after they became a member?
+
+WITH firstPurchase AS (
+	SELECT 
+		S.customer_id
+		,M.join_date
+	   ,S.order_date
+	   ,mn.product_name
+	   ,FIRST_VALUE(mn.product_name) OVER(PARTITION BY S.customer_id ORDER BY S.order_date) AS firstPurchasedItem
+	FROM sales AS S
+	LEFT JOIN members AS M
+		ON S.customer_id = M.customer_id
+	LEFT JOIN menu AS mn 
+	ON S.product_id = mn.product_id
+	WHERE M.join_date < S.order_date
+)
+SELECT DISTINCT
+	customer_id
+	,firstPurchasedItem
+FROM firstPurchase;
+
+
+-- 7. Which item was purchased just before the customer became a member?
+
+
+WITH purchasedItems AS (
+    SELECT 
+        S.customer_id,
+        S.order_date,
+        M.join_date,
+        S.product_id,
+        Menu.product_name,
+        ROW_NUMBER() OVER (PARTITION BY S.customer_id ORDER BY S.order_date) AS ranking
+    FROM sales AS S
+    LEFT JOIN members AS M ON S.customer_id = M.customer_id
+    LEFT JOIN menu AS Menu ON S.product_id = Menu.product_id
+    WHERE S.order_date < M.join_date
+)
+SELECT 
+    customer_id,
+    order_date,
+    join_date,
+    product_id,
+    product_name
+FROM purchasedItems
+WHERE ranking = 1;
+
