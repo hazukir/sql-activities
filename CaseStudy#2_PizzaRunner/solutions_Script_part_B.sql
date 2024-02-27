09aedca2be349653ebe7cfa93f70e066d6eccc89
Helder Varela <heldert4p@gmail.com>
2024-02-27 08:47:18 +0000

    Add solutions to question 2, part B of Pizza Runner

--- CaseStudy#2_PizzaRunner/solutions_Script_part_B.sql
+++ CaseStudy#2_PizzaRunner/solutions_Script_part_B.sql
@@ -1,7 +1,7 @@
 /*
 B. Runner and Customer Experience
 
-What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
 Is there any relationship between the number of pizzas and how long the order takes to prepare?
 What was the average distance travelled for each customer?
 What was the difference between the longest and shortest delivery times for all orders?
@@ -9,7 +9,7 @@ What was the average speed for each runner for each delivery and do you notice a
 What is the successful delivery percentage for each runner?
 */
 
--- How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+-- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
 
 WITH runnerSignups AS (
   SELECT -- Selecting runner_id, registration_date, and calculating startOfWeek
@@ -32,5 +32,28 @@ GROUP BY
   startOfWeek 
 ORDER BY 
   startOfWeek;
+
+
+
+
+-- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+SELECT 
+  ro.runner_id, 
+  AVG(
+    DATEDIFF(
+      MINUTE, 
+      CONVERT(DATETIME, co.order_time), 
+      TRY_CONVERT(DATETIME, ro.pickup_time) -- Because ro.pickup_time has 2 rows with NULL, I used this function
+    )
+  ) AS avg_minutes_to_pickup 
+FROM runner_orders AS ro 
+INNER JOIN customer_orders AS co 
+  ON ro.order_id = co.order_id 
+WHERE 
+  ro.pickup_time IS NOT NULL 
+  AND co.order_time IS NOT NULL -- Ensure order_time is not NULL
+GROUP BY 
+  ro.runner_id;
 
 
